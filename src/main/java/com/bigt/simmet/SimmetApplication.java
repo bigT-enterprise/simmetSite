bb078d1e5298d32693933307470d88ad24cf0a22
bgezkovski <bgezkovski@gmail.com>
2023-01-08 17:22:48 +0200

    Initial commit

---
+++ src/main/java/com/bigt/simmet/SimmetApplication.java
@@ -0,0 +1,13 @@
+package com.bigt.simmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimmetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimmetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigt/simmet/SimmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigt.simmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimmetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
